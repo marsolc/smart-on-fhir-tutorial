d5ef0201fc4d993df83fdb0aff1d2f32d406ebb1
marsolc <122798086+marsolc@users.noreply.github.com>
2023-02-07 14:06:32 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -14,7 +14,8 @@
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
-                      code:['http://loinc.org|8302-2', 'http://loinc.org|2085-9', 'http://loinc.org|2089-1', 'http://loinc.org|85354-9']
+                      code:['http://loinc.org|8302-2' , 'http://loinc.org|2085-9', 
+                            'http://loinc.org|2089-1' , 'http://loinc.org|85354-9']
                     }
                   });
        var allergy = smart.patient.api.fetchAll({
